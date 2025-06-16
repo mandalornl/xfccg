0c8bf38873bd3b7cb9c49a2ba7b0db2eb601268f
Joost Berns <joost.berns@gmail.com>
2025-06-16 07:09:52 +0200

    Signed in user menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+const user = useSupabaseUser();
+
 const year = computed(() => new Date().getFullYear());
 </script>
 
@@ -13,6 +15,49 @@ const year = computed(() => new Date().getFullYear());
           <app-logo />
         </nuxt-link>
       </v-toolbar-title>
+      <client-only>
+        <template #fallback>
+          <v-btn
+            :active="false"
+            to="/login"
+            icon="mdi-login"
+            color="primary"
+            title="Sign in"
+          />
+        </template>
+        <v-menu v-if="user">
+          <template #activator="{ props }">
+            <v-btn
+              variant="text"
+              color="primary"
+              icon="mdi-alien"
+              v-bind="props"
+            />
+          </template>
+          <v-list>
+            <v-list-item
+              to="/account"
+              title="Account"
+            />
+            <v-list-item
+              to="/my-decks"
+              title="My decks"
+            />
+            <v-list-item
+              to="/logout"
+              title="Log out"
+            />
+          </v-list>
+        </v-menu>
+        <v-btn
+          v-else
+          :active="false"
+          to="/login"
+          icon="mdi-login"
+          color="primary"
+          title="Sign in"
+        />
+      </client-only>
     </v-app-bar>
     <app-navigation />
     <v-main style="--v-layout-top: 64px">
