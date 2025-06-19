49c1ae84eb3f747b2879c9aa0189b4a4f82610b9
Joost Berns <joost.berns@softmedia.nl>
2025-06-19 16:09:55 +0200

    Only enable devtools when in development mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   ssr: true,
   devtools: {
-    enabled: true,
+    enabled: !production,
   },
   runtimeConfig: {
     public: {
