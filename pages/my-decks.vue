ac14e25797010a732b82dd0c0909df3655e7a9c8
Joost Berns <joost.berns@softmedia.nl>
2025-06-19 16:30:44 +0200

    Add my decks page

---
+++ pages/my-decks.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+definePageMeta({
+  middleware: [
+    'authenticated',
+  ],
+})
+</script>
+
+<template>
+  <layout-content title="My Decks">
+    TODO
+  </layout-content>
+</template>
