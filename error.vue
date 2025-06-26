4070a7b286e7c1e632adb5605d39b1c31fe5b58c
Joost Berns <joost.berns@softmedia.nl>
2025-06-26 14:59:15 +0200

    Added error page

---
+++ error.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+import type { NuxtError } from '#app';
+
+const props = defineProps<{
+  error?: NuxtError,
+}>();
+
+const message = computed(() => {
+  switch (props.error?.statusCode) {
+    case 400: return 'Bad Request';
+    case 401: return 'Unauthorized';
+    case 403: return 'Forbidden';
+    case 404: return 'Not Found';
+    default: return 'Internal Server Error';
+  }
+});
+
+watchEffect(() => {
+  if (!props.error) {
+    return;
+  }
+
+  useDebug(props.error);
+});
+
+const handleError = () => clearError({
+  redirect: '/'
+});
+</script>
+
+<template>
+  <v-container height="100vh">
+    <v-row
+      align="center"
+      justify="center"
+      class="fill-height"
+    >
+      <v-col>
+        <v-empty-state :headline="message">
+          <template #media>
+            <v-img
+              src="/logo.svg"
+              alt="The X-Files CCG"
+              class="mb-6"
+            />
+          </template>
+          <template #actions>
+            <v-btn
+              to="/"
+              variant="flat"
+              color="primary"
+              text="Go back home"
+              @click.prevent="handleError"
+            />
+          </template>
+        </v-empty-state>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
