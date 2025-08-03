26c892cc027f194db627b25f249b70a742de0225
Joost Berns <joost.berns@gmail.com>
2025-08-03 10:50:40 +0200

    Use enum type

--- app/components/CardType.vue
+++ app/components/CardType.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
+import { CardType as CardTypeEnum } from '~/types/card';
+
 const props = defineProps<{
   text: string,
 }>();
 
 const color = computed(() => ({
-  'Adversary': 'green-darken-2',
-  'Agent': 'white',
-  'Bluff': 'grey-darken-1',
-  'Combat': 'red-darken-4',
-  'Equipment': 'light-blue-darken-3',
-  'Event': 'pink-darken-2',
-  'Site': 'blue-darken-4',
-  'Witness': 'purple-darken-1',
-  'X-File': 'yellow',
+  [CardTypeEnum.Adversary]: 'green-darken-2',
+  [CardTypeEnum.Agent]: 'white',
+  [CardTypeEnum.Bluff]: 'grey-darken-1',
+  [CardTypeEnum.Combat]: 'red-darken-4',
+  [CardTypeEnum.Equipment]: 'light-blue-darken-3',
+  [CardTypeEnum.Event]: 'pink-darken-2',
+  [CardTypeEnum.Site]: 'blue-darken-4',
+  [CardTypeEnum.Witness]: 'purple-darken-1',
+  [CardTypeEnum.XFile]: 'yellow',
   'Credits': 'orange-darken-2',
 }[props.text]));
 </script>
