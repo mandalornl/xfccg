e1def955a6aa25bd776f9e3a22f0a1aa374c3179
Joost Berns <joost.berns@softmedia.nl>
2025-08-21 16:24:06 +0200

    Resources page

---
+++ app/components/ResourceCounter.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  color?: string,
+  rotate?: boolean,
+}>(), {
+  color: undefined,
+  rotate: false,
+});
+
+const counter = defineModel<number>({
+  default: 0,
+});
+</script>
+
+<template>
+  <v-card
+    :color="color"
+    :tile="rotate"
+    height="100%"
+    class="d-flex align-center"
+  >
+    <v-card-text :class="[ 'd-flex align-center justify-space-between', { 'flex-column fill-height': !rotate } ]">
+      <v-btn
+        :disabled="counter === 99"
+        :icon="rotate ? 'mdi-chevron-left' : 'mdi-chevron-up'"
+        color="white"
+        variant="text"
+        size="x-large"
+        @click="counter += 1"
+      />
+      <div :class="[ 'mx-4 text-h1 text-white font-weight-bold', { 'rotate-n90': rotate } ]">
+        {{ counter }}
+      </div>
+      <v-btn
+        :disabled="counter === 0"
+        :icon="rotate ? 'mdi-chevron-right' : 'mdi-chevron-down'"
+        color="white"
+        variant="text"
+        size="x-large"
+        @click="counter -= 1"
+      />
+    </v-card-text>
+  </v-card>
+</template>
+
+<style lang="scss" scoped>
+.rotate-n90 {
+  transform: rotate(-90deg);
+}
+</style>
--- app/pages/resources.vue
+++ app/pages/resources.vue
@@ -1,6 +1,112 @@
 <script setup lang="ts">
+const resource = ref<number>(5);
+const conspiracy = ref<number>(5);
+const wakeLock = ref<WakeLockSentinel>();
+const dialog = ref<boolean>(false);
+
+watch(dialog, async (value) => {
+  try {
+    if (value) {
+      wakeLock.value = await navigator.wakeLock.request('screen');
+
+      if (!document.fullscreenElement) {
+        await document.documentElement.requestFullscreen();
+      }
+    } else {
+      await wakeLock.value?.release();
+      wakeLock.value = undefined;
+
+      if (document.fullscreenElement && document.exitFullscreen) {
+        await document.exitFullscreen();
+      }
+    }
+  } catch (error) {
+    useDebug(error);
+  }
+});
+
+const reset = () => {
+  resource.value = 5;
+  conspiracy.value = 5;
+};
 </script>
 
 <template>
-  <div>Resources</div>
+  <layout-content title="Resources">
+    <div class="d-flex justify-end ga-2 mb-3">
+      <v-btn
+        text="Fullscreen"
+        variant="flat"
+        @click="dialog = true"
+      />
+      <v-btn
+        text="Reset"
+        variant="flat"
+        @click="reset"
+      />
+    </div>
+    <v-row no-gutters>
+      <v-col
+        cols="12"
+        sm="6"
+      >
+        <v-responsive aspect-ratio="1">
+          <resource-counter
+            v-model="resource"
+            color="blue"
+          />
+        </v-responsive>
+      </v-col>
+      <v-col
+        cols="12"
+        sm="6"
+      >
+        <v-responsive aspect-ratio="1">
+          <resource-counter
+            v-model="conspiracy"
+            color="red"
+          />
+        </v-responsive>
+      </v-col>
+    </v-row>
+    <v-dialog
+      v-model="dialog"
+      fullscreen
+    >
+      <v-btn
+        icon="mdi-close"
+        color="black"
+        variant="text"
+        class="position-absolute"
+        @click="dialog = false"
+      />
+      <v-row
+        no-gutters
+        class="flex-column fill-height"
+      >
+        <v-col>
+          <resource-counter
+            v-model="resource"
+            rotate
+            color="blue"
+          />
+        </v-col>
+        <v-col>
+          <resource-counter
+            v-model="conspiracy"
+            rotate
+            color="red"
+          />
+        </v-col>
+      </v-row>
+    </v-dialog>
+  </layout-content>
 </template>
+
+<style lang="scss" scoped>
+.position-absolute {
+  top: 4px;
+  left: 4px;
+  z-index: 1;
+}
+</style>
