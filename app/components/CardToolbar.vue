01bc92688c016d6612478335c2ccb40613dbb04d
Joost Berns <joost.berns@gmail.com>
2026-01-01 21:53:11 +0100

    Minor tweak

--- app/components/CardToolbar.vue
+++ app/components/CardToolbar.vue
@@ -18,67 +18,25 @@ const clickIcon = (event: Event) => {
     class="rounded-t position-sticky"
   >
     <div class="pa-3 flex-fill">
-      <v-menu
-        :close-on-content-click="false"
-        origin="overlap"
-        width="100%"
-      >
-        <template #activator="{ props:menuProps }">
-          <v-badge
-            :model-value="search !== ''"
-            dot
-            color="primary"
-            class="hidden-sm-and-up"
-          >
-            <v-btn
-              :active="search !== ''"
-              rounded
-              icon="mdi-magnify"
-              active-color="primary"
-              v-bind="menuProps"
-            />
-          </v-badge>
-        </template>
-        <v-list>
-          <v-list-item>
-            <v-text-field
-              :model-value="search"
-              flat
-              clearable
-              hide-details
-              autofocus
-              placeholder="Search"
-              prepend-inner-icon="mdi-magnify"
-              variant="solo-filled"
-              type="search"
-              @click:clear="search = ''"
-              @click:prepend-inner="clickIcon"
-              @focus="$event.target.select()"
-              @keydown.exact.enter="search = $event.target.value"
-            />
-          </v-list-item>
-        </v-list>
-      </v-menu>
-      <v-text-field
-        :model-value="search"
-        flat
-        clearable
-        hide-details
-        placeholder="Search"
-        prepend-inner-icon="mdi-magnify"
-        variant="solo-filled"
-        type="search"
-        class="hidden-xs"
-        @click:clear="search = ''"
-        @click:prepend-inner="clickIcon"
-        @focus="$event.target.select()"
-        @keydown.exact.enter="search = $event.target.value"
-      />
-    </div>
-    <template #append>
-      <div class="d-flex align-center ga-2 pa-3">
-        <slot />
+      <div class="d-flex flex-column flex-sm-row justify-sm-space-between">
+        <v-text-field
+          :model-value="search"
+          flat
+          clearable
+          hide-details
+          placeholder="Search"
+          prepend-inner-icon="mdi-magnify"
+          variant="solo-filled"
+          type="search"
+          @click:clear="search = ''"
+          @click:prepend-inner="clickIcon"
+          @focus="$event.target.select()"
+          @keydown.exact.enter="search = $event.target.value"
+        />
+        <div class="d-flex align-center mt-2 mt-sm-0 ml-sm-4 ga-2 pr-2">
+          <slot />
+        </div>
       </div>
-    </template>
+    </div>
   </v-toolbar>
 </template>
