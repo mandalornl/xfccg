399d909a784bca4d94a75794a3039ae3147ebe74
Joost Berns <joost.berns@gmail.com>
2026-01-07 15:03:17 +0100

    Cleanup

--- app/utils/shuffle.ts
+++ app/utils/shuffle.ts
@@ -1,5 +1,5 @@
 export function shuffle<T extends readonly unknown[]>(value: T): T[number][] {
-  const copy = [...value] as T[number][];
+  const copy = [ ...value ] as T[number][];
 
   for (let i = copy.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
