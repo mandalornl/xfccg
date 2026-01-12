5e6f839704d36e09d52d2485fe13d6ec1052bb11
Joost Berns <joost.berns@gmail.com>
2026-01-12 22:34:24 +0100

    Cleanup

--- app/composables/useDataTableSetup.ts
+++
@@ -1,9 +0,0 @@
-import type { DataTableSetup } from '~/types/datatable';
-
-export function useDataTableSetup<T>(key: string, setup: DataTableSetup<T>) {
-  const dataTableState = useDataTableState<T>(key);
-
-  if (setup.filters) {
-    dataTableState.value.filters = useDataTableFilters<T>(setup.filters);
-  }
-}
